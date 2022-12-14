4077ccba13e426c83639a55349d141fd50f78eb2
lthnh15032001 <lthnh15032001@gmail.com>
2022-12-15 00:13:20 +0700

    init php commit

---
+++ controller/list.php
@@ -0,0 +1,6 @@
+<?php 
+    include '../controller/common.php'; // common kiem tra session
+    include '../model/admin.php'; // model
+    $hieu = "123123123"; // controller
+    include '../views/admin.php' // view
+?>
---
+++ model/admin.php
@@ -0,0 +1,16 @@
+<?php
+include '../controller/database.php'; // 
+include_once 'check_session.php';
+$name_kinds = $_POST['name-kinds'];
+$QUERY  = "SELECT * FROM `table` ";
+function get()
+{
+}
+
+function add()
+{
+}
+
+function edit()
+{
+}
