64d6c7c638562b1a9b737b95af7e6a4e60a297c2
= <=>
2022-12-24 12:31:12 +0700

    remove duplicated common

--- app/controller/homepage.php
+++ app/controller/homepage.php
@@ -3,7 +3,6 @@ include '../controller/common.php'; // common kiem tra session
 include '../common/database.php'; // database kết nối -> bắt buộc mọi controller phải có dòng này
 include '../model/admin.php'; // model -> lựa chọn model phù hợp
 
-# ko có logic gì cần xử lý
 # xử lí logout ở đây
 if(isset($_POST['logout'])){
     unset($_SESSION['username']);
