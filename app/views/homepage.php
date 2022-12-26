13638c8f8e123832477a74b145e3f1cd67580ba4
lthnh15032001 <lthnh15032001@gmail.com>
2022-12-26 22:55:07 +0700

    FEAT: navbar chung cho project+controller môn học

--- app/views/homepage.php
+++ app/views/homepage.php
@@ -19,32 +19,9 @@
 </head>
 
 <body>
-    <div class="justify-content-between d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-        <div class="my-0 mr-md-auto font-weight-normal">
-            <div class="d-flex">
-                <span class="mb-1">Tên login <b><?php if (isset($_SESSION['username'])) {
-                                                    echo $_SESSION['username'];
-                                                } ?></b> </span>
-            </div>
-            <span>Thời gian login: <b> <?php if (isset($_SESSION['timelogin'])) {
-                                            echo $_SESSION['timelogin'];
-                                        } ?></b></span>
-        </div>
-        <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark" href="#">Giảng viên</a>
-            <a class="p-2 text-dark" href="#">Môn học</a>
-            <a class="p-2 text-dark" href="#">Thời khóa biểu</a>
-        </nav>
-        <div class="d-flex">
-            <div class="user_logout">
-                <!-- logout in php -->
-                <form action="" method="post">
-                    <button type="submit" name="logout" class="btn btn-danger">Đăng xuất</button>
-                </form>
-            </div>
-        </div>
-    </div>
-
+   <?php 
+    include '../common/navbar.php';
+   ?>
     <div class="container">
         <div class="card-body">
             <h2 class="card-title">Thời khóa biểu</h2>
@@ -76,7 +53,7 @@
                             <a href="">Tìm kiếm</a>
                         </li>
                         <li class="list-group-item list-group-item-custom">
-                            <a href="">Thêm mới</a>
+                            <a href="subject_add_input.php">Thêm mới</a>
                         </li>
                     </ul>
                 </div>
