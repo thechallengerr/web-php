7d55afaf427899f22ed3a611d8264a46fb4692cb
tranthuhien_k64a3 <tranthuhien_t64@hus.edu.vn>
2023-01-03 16:58:52 +0700

    feature: delete subject

--- app/controller/subject_search.php
+++ app/controller/subject_search.php
@@ -4,11 +4,16 @@ include '../common/database.php';
 include '../model/subject.php';
 
 $row = get_all_subjects(); 
-if (isset($_POST["subject_search"])) {
-    $school_year_value = $_POST["school_year"];
-    $keyword_value = $_POST["keyword"];
+if (isset($_GET["subject_search"])) {
+    $school_year_value = $_GET["school_year"];
+    $keyword_value = $_GET["keyword"];
     $row = get_some_subjects($school_year_value, $keyword_value); 
 } 
+
+if(isset($_GET["delete_subject"])) {
+    $id = $_GET["delete_subject"];
+    delete_subject($id);
+}
      
 
 include "../views/subject_search.php";
