7184bd3b95c79020752699111c850cfab6f1e2e5
DoHoang178 <dohoang1782000@gmail.com>
2023-01-14 10:35:32 +0700

    fix confirm

--- app/common/define.php
+++ app/common/define.php
@@ -5,3 +5,10 @@ define("SPECIALIZED",  array('001'=>'Khoa học máy tính', '002'=>'Khoa học 
 define("DEGREE", array('001'=>'Cử nhân', '002'=>'Thạc sĩ', '003'=>'Tiến sĩ','004'=>'Phó giáo sư', '005'=>'Giáo sư'));
 define("YEAR", array('001'=>'Năm 1','002'=>'Năm 2','003'=>'Năm 3','004'=>'Năm 4'));
 define("WEEKDAY", array('1'=>'Chủ Nhật','2'=>'Thứ 2','3'=>'Thứ 3','4'=>'Thứ 4','5'=>'Thứ 5','6'=>'Thứ 6','7'=>'Thứ 7',));
+
+
+define("SUBJECT", array('1'=>'Math'));
+define("TEACHER", array('1'=>'Nguyễn Văn An'));
+define("LESSION", array('1'=>'Tiết 1','2'=>'Tiết 2','3'=>'Tiết 3','4'=>'Tiết 4','5'=>'Tiết 5','6'=>'Tiết 6','7'=>'Tiết 7', '8'=>'Tiết 8', '9'=>'Tiết 9', '10'=>'Tiết 10'));
+define("NOTES", array('1'=>'Math'));
+
